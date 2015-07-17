3a1f20d07ea2d6d7a93d6c443ffc5a29ee227435
Alex Larrimore <j.alex.larrimore@gmail.com>
2015-07-17 12:07:06 -0700

    Added necessary sprites, gameObjects and Scripts. Just starting to code.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController Instance;
+
+	void Awake () {
+
+		if (Instance != null && Instance != this) {
+			DestroyImmediate(gameObject);
+		}
+
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+	
+	}
+
+	void Update () {
+	
+	}
+}
